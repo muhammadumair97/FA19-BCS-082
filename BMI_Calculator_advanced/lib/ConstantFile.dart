c0a8e1c4a32d27f23011e30f62780d9a3be7771b
Muhammad Umair <99950150+muhammadumair97@users.noreply.github.com>
2022-04-14 11:57:00 +0500

    Delete ConstantFile.dart

--- BMI_Calculator_advanced/lib/ConstantFile.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const activeColor= Color(0xF1d1E33);
-const deActiveColor= Color(0xF111328);
-const kLabelStyle =TextStyle(
-fontSize: 20.0,
-color: Colors.white,
-);
