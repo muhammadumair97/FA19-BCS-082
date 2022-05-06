3f5c1531355760cff1875c06dc30fe741357d2a0
muhammadumair97 <99950150+muhammadumair97@users.noreply.github.com>
2022-05-06 08:49:13 +0500

    created App bar
    app bar having project title

---
+++ Pocket_Password_Dairy/lib/main.dart
@@ -0,0 +1,20 @@
+ import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+     home: AppBar(
+       title: Text("Pocket Password Diary"),
+     ),
+      );
+
+  }
+}
