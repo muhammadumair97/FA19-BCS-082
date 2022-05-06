a70d5fa352a805b484f299cc9396f54ea8aba268
muhammadumair97 <99950150+muhammadumair97@users.noreply.github.com>
2022-05-06 11:18:19 +0500

    Update home_page.dart

--- Pocket_Password_Dairy/lib/home_page.dart
+++ Pocket_Password_Dairy/lib/home_page.dart
@@ -46,48 +46,49 @@ class HomePage extends StatelessWidget {
             ),
         ),
       ),
-      drawer: Drawer(
-        child: ListView(
-                   padding: EdgeInsets.zero,
-                   children: const <Widget>[
-                     UserAccountsDrawerHeader(
-                      accountName: Text("Muhammad Umair"),
-                        accountEmail: Text("FA19-BCS-082"),
-                       currentAccountPicture: CircleAvatar(
-                           backgroundImage: NetworkImage("https://www.google.com/url?sa=i&url=http%3A%2F%2Fzhongfeiqiao.com%2FStationery%2Fgoodslist%2Fid%2F760.html&psig=AOvVaw0QavG1__8hRc4AiSQhR5_V&ust=1651902075277000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCHlviUyvcCFQAAAAAdAAAAABAD")
-                       ),
-                     ),
+        drawer: Drawer(
+          child: ListView(
+              padding: EdgeInsets.zero,
+              children: const <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text("Muhammad Umair"),
+                  accountEmail: Text("FA19-BCS-082"),
+                  currentAccountPicture: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://www.google.com/url?sa=i&url=http%3A%2F%2Fzhongfeiqiao.com%2FStationery%2Fgoodslist%2Fid%2F760.html&psig=AOvVaw0QavG1__8hRc4AiSQhR5_V&ust=1651902075277000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMCHlviUyvcCFQAAAAAdAAAAABAD")
+                  ),
+                ),
 
-                     ListTile(
-                       leading: Icon(Icons.email_outlined),
-                       title: Text("Email"),
-                       subtitle: Text("umairaltaf727@gmail.com"),
-                       trailing: Icon(Icons.send),
+                ListTile(
+                  leading: Icon(Icons.email_outlined),
+                  title: Text("Email"),
+                  subtitle: Text("umairaltaf727@gmail.com"),
+                  trailing: Icon(Icons.send),
 
-                     ),
-
-
-                     ListTile(
-                       leading: Icon(Icons.feedback_outlined),
-                       title: Text("Feedback"),
-                       subtitle: Text("just do it"),
-                       trailing: Icon(Icons.send),
-
-                     ),
+                ),
 
 
-                     ListTile(
-                       leading: Icon(Icons.home),
-                       title: Text("About Us"),
-                       subtitle: Text("-----------"),
-                       trailing: Icon(Icons.send),
+                ListTile(
+                  leading: Icon(Icons.feedback_outlined),
+                  title: Text("Feedback"),
+                  subtitle: Text("just do it"),
+                  trailing: Icon(Icons.send),
 
-                     ),
-    ]
+                ),
 
-      ),
 
-    )
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("About Us"),
+                  subtitle: Text("-----------"),
+                  trailing: Icon(Icons.send),
+
+                ),
+              ]
+
+          ),
+
+        )
     );
   }
 }
