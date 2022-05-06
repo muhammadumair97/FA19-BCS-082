72cdd1dac1e7ba662d8ff76e67e9c3d56d45acfd
muhammadumair97 <99950150+muhammadumair97@users.noreply.github.com>
2022-05-06 17:04:01 +0500

    created a seperate page to generate simple password
    just added a password hint

---
+++ Pocket_Password_Dairy/lib/simple_password.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class simplepassword extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Expanded(child:Text("Password Hint :     abcd1234"))
+        ],
+      ),
+    );
+  }
+}
+
+
+ 
