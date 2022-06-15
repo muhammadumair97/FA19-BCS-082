b91da6d35698eff39770b2e31d0577293ccdcb0f
muhammadumair97 <99950150+muhammadumair97@users.noreply.github.com>
2022-06-15 10:33:00 +0500

    add validations

--- cometti_app/lib/create_new_group.dart
+++ cometti_app/lib/create_new_group.dart
@@ -6,8 +6,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'model/group_model.dart';
 
 
-class group extends StatelessWidget {
 
+class group extends StatefulWidget {
+
+  @override
+  State<group> createState() => _groupState();
+}
+
+class _groupState extends State<group> {
 
 
   @override
@@ -18,7 +24,16 @@ class group extends StatelessWidget {
         appBar: AppBar(iconTheme: IconThemeData(
           color: Colors. black, //change your color here.
         ),
-            title: const Text("Create New Group"),
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+
+            // back to login form
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+            title: const Text("Create New Cometti"),
           centerTitle: true,
         ),
         body: const MyStatefulWidget(),
@@ -35,90 +50,218 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+  // form key
+  final _formKey =GlobalKey<FormState>();
+
+   //final _auth = FirebaseAuth.instance;
+
+
   TextEditingController idController = TextEditingController();
   TextEditingController nameController = TextEditingController();
   TextEditingController amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(                     //    add picture  for creating new group
-                  'Welcome',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-
-
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: idController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Group ID',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Group Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: amountController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Amount per head',
-                ),
-              ),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                child: ElevatedButton(
-                  child: const Text('Create'),
-                  onPressed: () {
-                    postDetailsToFirestore();
-
-                  },
-                )
-            ),
-    Container(
-    height: 50,
-    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-
-    child: ElevatedButton(
-    child: const Text('Back'),
-    onPressed: () {
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => const Admin()
-    )
-    );
-    })
-    )],
+    // group id
+    final idField = TextFormField(
+        autofocus: false,
+        controller: idController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = RegExp(r'^[a-zA-Z0-9]{3,}$');
+          if (value!.isEmpty) {
+            return ("Group id can not be empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter valid id (min. 3 characters)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          idController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.account_circle),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Group ID",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            )
         ));
+
+    // group name
+    final groupNameField = TextFormField(
+        autofocus: false,
+        controller: nameController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = RegExp(r'^[a-zA-Z]{3,}$');
+          if (value!.isEmpty) {
+            return ("Group name can not be empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter valid  Group Name (min. 3 characters)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          nameController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.account_circle),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Group Name",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            )
+        ));
+
+
+    //amount per head
+
+    final AmountField = TextFormField(
+        autofocus: false,
+        controller: amountController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = RegExp(r'^[0-9]{2,}$');
+          if (value!.isEmpty) {
+            return ("Amount can not be empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter valid Amount (min. 2 digits)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          amountController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.account_circle),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Amount",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            )
+        ));
+
+
+    //create button
+    //signup button
+    final createButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.blueAccent,
+
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery
+            .of(context)
+            .size
+            .width,
+        onPressed: () {
+          creategroup(idController.text, nameController.text ,amountController.text);
+        },
+        child: Text("Create", textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+
+    );
+
+
+    // back button
+    final backButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.blueAccent,
+
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery
+            .of(context)
+            .size
+            .width,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Admin()
+              )
+          );
+        },
+        child: Text("Back", textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+
+          child: SingleChildScrollView(
+              child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(36.0),
+
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+
+                          children: <Widget>[
+
+                            SizedBox(height: 45),
+                            idField,
+                            SizedBox(height: 20),
+                            groupNameField,
+                            SizedBox(height: 20),
+                            AmountField,
+                            SizedBox(height: 20),
+                            createButton,
+                            SizedBox(height: 20),
+                            backButton,
+                            SizedBox(height: 20),
+
+                          ]
+
+                      ),
+                    ),
+                  )
+              )
+          )
+
+      ),
+    );
   }
 
 
+  void creategroup( String id , String name, String amount ,) async
+  {
+    if (_formKey.currentState!.validate()) {
 
-//firebase code
+      await _auth.group(id:id, name: name, amount: amount,)
+          .then((value) => {
+            postDetailsToFirestore()
+      }).catchError((e)
+      {
+        Fluttertoast.showToast(msg: e!.message);
+      });
+    }
+  }
+
+  //firebase code
 
 
   postDetailsToFirestore() async {
@@ -126,9 +269,6 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
     //calling our user model
     // calling these values
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
-
-
-
     GroupModel groupModel = GroupModel();
 
     // writing all the values
@@ -146,4 +286,10 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   }
 
+
 }
+
+class _auth {
+  static group({required String id, required String name, required String amount}) {}
+}
+
