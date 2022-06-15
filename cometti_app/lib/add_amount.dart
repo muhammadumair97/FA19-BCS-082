68d73d3909e1b23e71588af14a098370a97a1361
muhammadumair97 <99950150+muhammadumair97@users.noreply.github.com>
2022-06-15 10:34:58 +0500

    updated
    changes the class from stateless to  statefull  and updated the textbox designs

--- cometti_app/lib/add_amount.dart
+++ cometti_app/lib/add_amount.dart
@@ -4,10 +4,17 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 import 'admin.dart';
 import 'model/amount.dart';
-class amount extends StatelessWidget {
+class amount extends StatefulWidget {
+  const amount({Key? key}) : super(key: key);
 
 
 
+
+  @override
+  State<amount> createState() => _amountState();
+}
+
+class _amountState extends State<amount> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,6 +24,15 @@ class amount extends StatelessWidget {
             iconTheme: IconThemeData(
               color: Colors. black, //change your color here.
             ),
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+
+            // back to login form
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
             title: const Text("Add Amount"),
           centerTitle: true,
         ),
@@ -34,6 +50,11 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
+
+  final _formKey =GlobalKey<FormState>();
+
+
   TextEditingController idController = TextEditingController();
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
@@ -41,84 +62,216 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: idController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Group ID',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Name',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Email Address',
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: amountController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Amount',
-                ),
-              ),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                child: ElevatedButton(
-                  child: const Text('Add'),
-                  onPressed: () {
-                    postDetailsToFirestore();
-                  },
-                )
-            ),
-
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-
-                child: ElevatedButton(
-                    child: const Text('Back'),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const Admin()
-                          )
-                      );
-                    })
+    //group id
+    final idField = TextFormField(
+        autofocus: false,
+        controller: idController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = RegExp(r'^[a-zA-Z]{3,}$');
+          if (value!.isEmpty) {
+            return ("Group id can not be empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter valid id (min. 3 characters)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          idController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.account_circle),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Group ID",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
             )
-          ],
         ));
+
+    //group name
+    final nameField = TextFormField(
+        autofocus: false,
+        controller: nameController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = RegExp(r'^[a-zA-Z]{3,}$');
+          if (value!.isEmpty) {
+            return ("Group name can not be empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter valid group name (min. 3 characters)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          nameController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.account_circle),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Group Name",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            )
+        ));
+
+    //email address
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return "Please Enter your Email";
+          }
+
+          // reg expressions for email validations
+          if (!RegExp("^[a-zA-Z0-9+_.]+@[a-z]+.[a-z]").hasMatch(
+              value)) {
+            return ("Please enter a valid Email");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          emailController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.account_circle),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Email Address",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            )
+        ));
+
+    //amount
+    final amountField = TextFormField(
+        autofocus: false,
+        controller: amountController,
+        keyboardType: TextInputType.name,
+        validator: (value) {
+          RegExp regex = RegExp(r'^[0-9]{2,}$');
+          if (value!.isEmpty) {
+            return ("Amount can not be empty");
+          }
+          if (!regex.hasMatch(value)) {
+            return ("Enter valid Amount (min. 2 characters)");
+          }
+          return null;
+        },
+        onSaved: (value) {
+          amountController.text = value!;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.account_circle),
+            contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+            hintText: "Amount",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            )
+        ));
+
+    //add button
+    final addButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.blueAccent,
+
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery
+            .of(context)
+            .size
+            .width,
+        onPressed: () {
+          postDetailsToFirestore();
+        },
+        child: Text("Add", textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+
+    );
+
+    //back button
+    final backButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.blueAccent,
+
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery
+            .of(context)
+            .size
+            .width,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const Admin()
+              )
+          );
+        },
+        child: Text("Back", textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+
+    );
+
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+
+          child: SingleChildScrollView(
+              child: Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(36.0),
+
+                    child: Form(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+
+                          children: <Widget>[
+
+                            SizedBox(height: 45),
+                            idField,
+                            SizedBox(height: 20),
+                            nameField,
+                            SizedBox(height: 20),
+                            emailField,
+                            SizedBox(height: 20),
+                            amountField,
+                            SizedBox(height: 20),
+                            addButton,
+                            SizedBox(height: 20),
+                            backButton,
+                            SizedBox(height: 20),
+
+                          ]
+
+                      ),
+                    ),
+                  )
+              )
+          )
+
+      ),
+    );
   }
-
-
 // firebase code
-
 
   postDetailsToFirestore() async {
     //calling our firestore
@@ -145,6 +298,5 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         (context), MaterialPageRoute(builder: (context) => Admin()), (
         route) => false);
   }
-
 
 }
