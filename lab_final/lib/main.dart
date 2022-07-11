40b50943c790f6e7c21a79f6e75d9a49629cb12b
muhammadumair97 <99950150+muhammadumair97@users.noreply.github.com>
2022-07-11 09:50:26 +0500

    just link the login page as a start page
    link login page as main page

---
+++ lab_final/lib/main.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/material.dart';
+
+import 'manager/login.dart';
+
+
+
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Manager login',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const login(),
+
+    );
+  }
+}
